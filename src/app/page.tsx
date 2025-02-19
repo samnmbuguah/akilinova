ea2b9199c958bfc7afde379fce75f50169b3b7c6
Samuel Nyambura <samnmbuguah@gmail.com>
2025-02-19 14:07:26 +0000

    🔥 Remove Footer import from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from '../components/Hero';
 import Features from '../components/Features';
 import Testimonials from '../components/Testimonials';
 import Contact from '../components/Contact';
-import Footer from '../components/Footer';
 
 export default function Home() {
   return (
